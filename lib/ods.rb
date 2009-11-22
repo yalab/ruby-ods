9d0a5e381e40c5297e390fc164e3373f334c7599
yalab <yoshida@ya-lab.org>
2009-11-23 00:29:11 +0900

    lib/ods.rb: It came to be able to modify sheet name.

--- lib/ods.rb
+++ lib/ods.rb
@@ -1,18 +1,34 @@
 require 'rexml/document'
 require 'rubygems'
 require 'zip/zip'
-
+require 'fileutils'
 
 class Ods
   attr_reader :content, :sheets
+  XPATH_SHEETS = 'office:body/office:spreadsheet/table:table'
 
   def initialize(path)
-    Zip::ZipFile.open(path) do |zip|
+    @path = path
+    Zip::ZipFile.open(@path) do |zip|
       @content = REXML::Document.new zip.read('content.xml')
     end
     @sheets = []
-    @content.root.get_elements('office:body/office:spreadsheet/table:table').each do |sheet|
+    @content.root.get_elements(XPATH_SHEETS).each do |sheet|
       @sheets.push(Sheet.new(sheet))
+    end
+  end
+
+  def save(dest=nil)
+    if dest
+      FileUtils.cp(@path, dest)
+    else
+      dest = @path
+    end
+
+    Zip::ZipFile.open(dest) do |zip|
+      zip.get_output_stream('content.xml') do |io|
+        @content.write(io)
+      end
     end
   end
 
@@ -23,6 +39,10 @@ class Ods
 
     def name
       @content.attribute('name').to_s
+    end
+
+    def name=(name)
+      @content.add_attribute('table:name', name)
     end
   end
 end
--- test/ods_test.rb
+++ test/ods_test.rb
@@ -3,8 +3,15 @@ require 'test/unit'
 require File.dirname(File.expand_path(__FILE__)) + '/../lib/ods'
 
 class OdsTest < Test::Unit::TestCase
+  BASE_DIR = File.dirname(File.expand_path(__FILE__))
   def setup
-    @ods = Ods.new(File.dirname(File.expand_path(__FILE__)) + '/cook.ods')
+    @ods = Ods.new(BASE_DIR + '/cook.ods')
+  end
+
+  def teardown
+    if File.exists?(BASE_DIR + '/modified.ods')
+      File.unlink(BASE_DIR + '/modified.ods')
+    end
   end
 
   def test_sheet_count
@@ -13,5 +20,17 @@ class OdsTest < Test::Unit::TestCase
 
   def test_sheet_name
     assert_equal 'さつま揚げとキャベツのお味噌汁', @ods.sheets[0].name
+  end
+
+  def test_sheet_name_modify
+    modified_name = 'hogehoge'
+    offset = 2
+    file_path = BASE_DIR + '/modified.ods'
+
+    assert_not_equal modified_name, @ods.sheets[offset].name
+    @ods.sheets[offset].name = modified_name
+    @ods.save(file_path)
+    modified_ods = Ods.new(file_path)
+    assert_equal modified_name, modified_ods.sheets[offset].name
   end
 end
